fd3f341a454f07f49eb4d22587eba03fd25a049b
w6tsang <w.tsang96@gmail.com>
2015-05-20 18:48:29 -0400

    Create StartScript.cs

---
+++ scripts/StartScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)){
+			Application.LoadLevel("Level1");
+		}
+	}
+}
